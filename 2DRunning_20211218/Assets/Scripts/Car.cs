7cd3175d88ce8af3d3410bd91a9f775638e45a75
kaihvi00 <56634789+kaihvi00@users.noreply.github.com>
2022-01-08 19:44:47 +0800

    4大資料類型

--- 2DRunning_20211218/Assets/Scripts/Car.cs
+++ 2DRunning_20211218/Assets/Scripts/Car.cs
@@ -1,6 +1,23 @@
-using UnityEngine;
+using UnityEngine; // 引用Unity 引擎 讓我們可以使用 API
 
+// 類別 類別名稱 需大小寫一致
 public class Car : MonoBehaviour
 {
+    //此類別內容
 
+    #region 欄位 Field
+    // ※ 欄位語法
+    // 資料類型 欄位名稱 結尾
+    // 四大常用資料類型
+    // 整  數 ing
+    // 浮點數 float
+    // 字  串 string
+    // 布林值 bool
+    int cc;
+    float weight;
+    string brand;
+    bool hasWindow;
+    #endregion
 }
+
+//非類別類內容
